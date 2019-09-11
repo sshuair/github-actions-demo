b512b7f496268f70987ac6d3d4f502bde8b3a410
sshuair <sshuair@gmail.coom>
2019-09-11 21:37:57 +0800

    build wheels

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import re
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+with open("actions_demo/__init__.py", encoding="utf-8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+requirements = [x.strip() for x in open("requirements.txt").readlines()]
+
+setup(
+    name="torchsat",
+    version=version,
+    author="sshuair",
+    author_email="sshuair@gmail.com",
+    url="https://github.com/sshuair/github-actions-demo",
+    description="github actions for test, build, package, docker...",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=requirements,
+)
